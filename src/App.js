3d9c26e0c7fb6d2d06a135df4ca195482799e769
A1R1SZ <ismairizzrizal@gmail.com>
2024-03-01 10:57:07 +0800

    This is basic website form using react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import { Box, Button, Tab, TableContainer, TextField ,Paper, Table, TableHead, TableRow, TableCell, TableBody ,Radio,RadioGroup,FormControlLabel,FormControl,FormLabel } from '@mui/material';
+import { TabList,TabPanel,TabContext } from '@mui/lab';
+
+function TableSection(){
+
+}
 
 function App() {
+  const [name,setName]=useState("");
+  const [email,setEmail]=useState("");
+  const[address,setAddress]=useState("");
+  const[contactNo,setContactSet]=useState("");
+  const[age,setAge]=useState("");
+  const[gender,setGender]=useState("");
+  const [list,setList]=useState([]);
+  const[value,setValue]=React.useState('1');
+  
+  const handleTabChange = (event,newValue)=>{
+    setValue(newValue);
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(name,email,address,contactNo,age,gender)
+    const date={name,email,address,contactNo,age,gender}
+    if(name&&email&&address&&contactNo&&age&&gender){
+      setList((ls)=>[...ls,date])
+      setName("")
+      setEmail("")
+      setAddress("")
+      setContactSet("")
+      setAge("")
+      setGender("")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      <TabContext value={value}>
+      <Box sx={{borderBottom:1,borderColor:'purple'}}>
+        <TabList onChange={handleTabChange} centered textColor='secondary'>
+          <Tab label='Form' value={'1'}/>
+          <Tab label="Table" value={'2'}/>
+        </TabList>
+      </Box>
+      <TabPanel value='1' sx={{display:'block'}}>
+        <TextField variant='outlined' name='name' placeholder='Name' value={name} onChange={(e)=>setName(e.target.value)} color='secondary' sx={{width:'40%', margin:'25px'}}/>
+        <TextField variant='outlined' name='email' placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)} color='secondary' sx={{width:'40%' ,margin:'25px'}}/>
+        <TextField variant='outlined' name='address' placeholder='Address' value={address} onChange={(e)=>setAddress(e.target.value)} color='secondary' sx={{width:'40%' ,margin:'25px'}}/>
+        <TextField variant='outlined' name='contactNo' placeholder='ContactNo' value={contactNo} onChange={(e)=>setContactSet(e.target.value)} color='secondary' sx={{width:'40%' ,margin:'25px'}}/>
+        <TextField variant='outlined' name='age' placeholder='Age' value={age} onChange={(e)=>setAge(e.target.value)} color='secondary' sx={{width:'40%' ,margin:'25px'}}/>
+        {/* <TextField variant='outlined' name='gender' placeholder='Gender' value={gender} onChange={(e)=>setGender(e.target.value)} color='secondary' sx={{width:'40%' ,margin:'25px'}}/> */}
+        <FormControl>
+          <FormLabel id="gender-radio">Gender</FormLabel>
+          <RadioGroup name='radio-button-group' onChange={(e)=>setGender(e.target.value)} row>
+            <FormControlLabel value={"female"} control={<Radio />} label="Female" />
+            <FormControlLabel value={"male"} control={<Radio />} label="Male" />
+          </RadioGroup>
+        </FormControl>
+        <br/>
+        <Button variant='contained' onClick={handleSubmit}>Add To List</Button>
+      </TabPanel>
+      <TabPanel value='2'>
+        <TableSection/>
+          <TableContainer component={Paper}>
+          <Table sx={{minWidth:700}}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align='right'>Email</TableCell>
+                <TableCell align='right'>Address</TableCell>
+                <TableCell align='right'>ContactNo.</TableCell>
+                <TableCell align='right'>Age</TableCell>
+                <TableCell align='right'>Gender</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {list.map((lists)=>(
+                <TableRow key={lists.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope='lists'>
+                  {lists.name}
+                </TableCell>
+                <TableCell align='right'>{lists.email}</TableCell>
+                <TableCell align='right'>{lists.address}</TableCell>
+                <TableCell align='right'>{lists.contactNo}</TableCell>
+                <TableCell align='right'>{lists.age}</TableCell>
+                <TableCell align='right'>{lists.gender}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </TabPanel>
+     </TabContext>
+    </Box>
   );
 }
 
